7b6042ffe3362e0a1f7edfd741a8427af23ee546
ebrahim ayyad <ayyad_ebrahim@hotmail.com>
2021-08-09 01:10:34 +0300

    ..

---
+++ pages/overview.js
@@ -0,0 +1,10 @@
+import next from "next"
+import Link from 'next/link'
+
+export default function OverView(){
+    return(
+        <Link href="/">
+            <a>Return to Main page</a>
+        </Link>
+    )
+}
